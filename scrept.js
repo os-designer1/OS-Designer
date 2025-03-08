20dc1ec7269bd473f76e7a30ae6600b678b1f3ee
os-designer1 <Gitos5647@gmail.com>
2025-03-08 15:30:46 -0800

    .

--- scrept.js
+++ scrept.js
@@ -12,3 +12,44 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 3000, // تغيير كل 3 ثوانٍ
+        disableOnInteraction: false, // يستمر في العمل بعد التفاعل
+    },
+    speed: 1000, // سرعة التبديل
+    });
+    
+    swiper.on('slideChangeTransitionStart', function () {
+    var activeSlide = document.querySelector('.swiper-slide-active .overlay');
+    if (activeSlide) {
+        activeSlide.style.opacity = '0';
+        activeSlide.style.transition = 'opacity 0.5s ease-in-out';
+    }
+    });
+    
+    swiper.on('slideChangeTransitionEnd', function () {
+    var activeSlide = document.querySelector('.swiper-slide-active .overlay');
+    if (activeSlide) {
+        activeSlide.style.opacity = '1';
+        activeSlide.style.transition = 'opacity 0.5s ease-in-out';
+    }
+    });
+    
+    // Adjust image sources for mobile devices
+    swiper.on('init', function () {
+    var slides = document.querySelectorAll('.swiper-slide picture source');
+    slides.forEach(function (slide) {
+        if (window.innerWidth <= 767) {
+            slide.srcset = slide.srcset.replace('img-', 'image-small');
+        }
+    });
+    });
+    
+    swiper.init();
